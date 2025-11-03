3c515a0528cc3da84024959604eca8410c801efd
runtome <suphot.n@gmail.com>
2025-11-03 22:46:14 +0700

    Add Ripple effect

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -7,7 +7,11 @@ import {
 
 function GoalItem(props: { text: string; id: string; onDeleteItem: (id: string) => void }) {
   return (
-    <Pressable onPress={() => props.onDeleteItem(props.id)}>
+    <Pressable 
+        android_ripple={{color:'#dddddd'}}
+        onPress={() => props.onDeleteItem(props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+    >
         <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
         </View>
@@ -20,11 +24,17 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
+  
+  pressedItem: {
+    opacity: 0.5,
+  },
+
   goalText: {
     color: 'white',
+    padding: 8,
+
   },
 });
