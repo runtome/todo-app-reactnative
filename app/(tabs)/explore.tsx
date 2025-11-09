2708a904855216ec0cfbb10a4d578b5e536cbe1a
runtome <suphot.n@gmail.com>
2025-11-09 22:12:28 +0700

    style adjust

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -8,6 +8,7 @@ import {
 
 import GoalInput from '@/components/GoalInput';
 import GoalItem from '@/components/GoalItem';
+import { StatusBar } from 'expo-status-bar';
 
 export default function TabTwoScreen() {
   const [modalIsVisible, setModalIsVisible] = useState<boolean>(false);
@@ -37,33 +38,36 @@ export default function TabTwoScreen() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title='Add new Goal'
-        color='#5e0acc'
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput 
-      visible={modalIsVisible} 
-      onAddGoal={addGoalHandler}
-      onCancel={endAddGoalHandler} />
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return <GoalItem 
-            text={itemData.item.text}
-            id={itemData.item.id}
-            onDeleteItem={deleteGoalHandler}
-            />;
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
+    <>
+      <StatusBar style='light'/>
+      <View style={styles.appContainer}>
+        <Button
+          title='Add new Goal'
+          color="#a065ec"
+          onPress={startAddGoalHandler}
         />
+        <GoalInput 
+        visible={modalIsVisible} 
+        onAddGoal={addGoalHandler}
+        onCancel={endAddGoalHandler} />
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              return <GoalItem 
+              text={itemData.item.text}
+              id={itemData.item.id}
+              onDeleteItem={deleteGoalHandler}
+              />;
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -72,6 +76,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+    backgroundColor: '#1e085a'
   },
   inputContainer: {
     flex: 1,
